ec2872d5ea85c6d81105303b29e8561d95a06325
Ken <coofree@foxmail.com>
2017-06-23 14:34:32 +0800

    1

---
+++ CFCustomTableView/CFCustomTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CFCustomTableView
+//
+//  Created by YF on 2017/6/23.
+//  Copyright © 2017年 CooFree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
